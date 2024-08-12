08d708e826d9849bb038d0993a46a098a61d0608
Amruthraj-coder <amruthraj1952@gmail.com>
2024-08-12 12:27:51 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect,useRef } from "react";
+import { Button } from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+let arr =[]
+const App = ()=>{
+    let obj=useRef({name:"amruthraj",age:20});
+    let btnref=useRef(null)
+    let func =()=>{setCount(count+1)
+        arr.push(obj)
+        if(arr.length===2){
+            
+            console.log(arr[0]===arr[1]);
+        }
+    }
+    console.log(arr[0],arr[1]); 
+    let incrementage = ()=>{
+        obj.current.age+=1
+    }
+    useEffect(()=>{
+        console.log(btnref.current.innerText);
+    })
+const [count,setCount] =useState(0);
+    return (<>
+    <h1>count:{count}</h1>
+    <p>name:<b>{obj.current.name}</b> <i>age:{obj.current.age}</i></p>
+    <Button onClick={func} >Increment</Button>
+    <button ref={btnref} onClick={incrementage}>increment age</button>
+    </>)
 }
 
-export default App;
+ export default App;
+
+
+
